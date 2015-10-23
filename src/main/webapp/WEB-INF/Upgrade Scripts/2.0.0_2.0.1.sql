45b003b202266128c209902aa0b507a11c2cd751
tuxming <tuxming@gmail.com>
2015-10-23 17:07:24 +0800

    sql update

--- src/main/webapp/WEB-INF/Upgrade Scripts/2.0.0_2.0.1.sql
+++ src/main/webapp/WEB-INF/Upgrade Scripts/2.0.0_2.0.1.sql
@@ -1,8 +1,8 @@
 -- Update exceptions template
-UPDATE jnoc_uat.EMAILTEMPLATE SET `HTML` = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml"> <head> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> </head> <body> #foreach($variable in $variablesList) <br /> DATE: $variable.date <br /> TIME: $variable.time <br /> EXCEPTION NAME: $variable.exception_name <br /> EXCEPTION MESSAGE: $variable.exception_message <br /> EXCEPTION STACK: $variable.exception_stack <br /> #end </body> </html>' WHERE ID = 'EXCEPTION';
+UPDATE EMAILTEMPLATE SET `HTML` = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml"> <head> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> </head> <body> #foreach($variable in $variablesList) <br /> DATE: $variable.date <br /> TIME: $variable.time <br /> EXCEPTION NAME: $variable.exception_name <br /> EXCEPTION MESSAGE: $variable.exception_message <br /> EXCEPTION STACK: $variable.exception_stack <br /> #end </body> </html>' WHERE ID = 'EXCEPTION';
 
 -- Insert first permission
-INSERT INTO jnoc_uat.`PERMISSION` (ID, `NAME`, VERSION) 
+INSERT INTO `PERMISSION` (ID, `NAME`, VERSION) 
 	VALUES ('1', '*:*', 1);
-INSERT INTO jnoc_uat.SUBJECT_PERMISSION (Subject_UID, permissions_ID) 
+INSERT INTO SUBJECT_PERMISSION (Subject_UID, permissions_ID) 
 	VALUES ('1', '1');
